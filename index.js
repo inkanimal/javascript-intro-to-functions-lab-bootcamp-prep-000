521dcae09358b1dae78979859ab5e75c830115e1
geoffrey woodcock <theatomproject@gmail.com>
2017-10-16 19:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string){
   return string.toLowerCase()
 }
 function sayHiToGrandma(string){
-  if string.toLowerCase()
   console.log("I can't hear you!")
+  return string.toLowerCase()
 
 }
